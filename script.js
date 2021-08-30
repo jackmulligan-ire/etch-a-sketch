3a0193668e7e9e8855f8d20fc8007a0ba327e197
Jack Mulligan <mulligja1@gmail.com>
2021-08-30 15:24:01 +0100

    16x16 version created

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const container = document.querySelector('#container');
-const squareElem = document.createElement('div');
-squareElem.classList.add('square');
-squareElem.addEventListener("mouseover", () => squareElem.classList.add('black'))
-container.appendChild(squareElem);
+const numOfSquares = 256;
+
+let squareElem;
+for (i=1; i <= numOfSquares; i++) {
+    squareElem = document.createElement('div');
+    squareElem.classList.add('square');
+    container.appendChild(squareElem);
+}
+const squares = document.querySelectorAll('.square');
+squares.forEach((square) => 
+    square.addEventListener("mouseover", () => square.classList.add('black')))
+
