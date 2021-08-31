2a7fe40b18a5daa710af0fab64e76b719072624f
Jack Mulligan <mulligja1@gmail.com>
2021-08-31 13:20:18 +0100

    Completed reset feature

--- script.js
+++ script.js
@@ -1,31 +1,26 @@
 const container = document.querySelector('#container');
 const resetButton = document.getElementById('reset-button');
 const initDimensions = 16;
-let squares; 
+
 drawGrid(initDimensions)
 resetButton.addEventListener("click", resetGrid)
 
 function drawGrid(dimensions) {
     let numOfSquares = Math.pow(dimensions, 2);
-    // Declare flexValue = 100/dimensions.toFixed(3);
     let flexValue = 100/dimensions.toFixed(3);
-    // Declare flexDeclaration = flexValue + "%"
     let flexDeclaration = flexValue + "%";
-    // Declare var squareStyle
-    let squareElem
+    let square
     for (i=1; i <= numOfSquares; i++) {
-        squareElem = document.createElement('div');
-        squareElem.classList.add('square');
-        // declare flex of squareElem to be flexDeclaration
-        squareElem.style.flex = flexDeclaration;
-        //squareElem.textContent = `${i}`; //for seeing problems with columns and rows
-        container.appendChild(squareElem);
+        square = document.createElement('div');
+        square.classList.add('square');
+        square.style.flex = flexDeclaration;
+        square.addEventListener("mouseover", (e) => e.target.classList.add('black'));
+        container.appendChild(square);
     }
-    squares = document.querySelectorAll('.square');
-    squares.forEach((square) => 
-        square.addEventListener("mouseover", () => square.classList.add('black')));
 }
+
 function resetGrid() {
+    let squares = document.querySelectorAll('.square');
     squares.forEach(square => container.removeChild(square));
     let userDimensions = +prompt("Enter in dimensions of the grid (max 100: ", 16);
     if (userDimensions > 100) userDimensions = 100;
