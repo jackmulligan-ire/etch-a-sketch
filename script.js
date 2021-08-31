075d1137f97b9ad929521b20006d5b1bd9c895c5
Jack Mulligan <mulligja1@gmail.com>
2021-08-31 08:49:08 +0100

    User can now specify dimensions

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
 const container = document.querySelector('#container');
-// Get the button element and store in a const resetButton
 const resetButton = document.getElementById('reset-button');
-const numOfSquares = 256;
+const initDimensions = 16;
+drawGrid(initDimensions)
+let squares = document.querySelectorAll('.square');
+// Adding the event listener for the initial squares
+squares.forEach((square) => 
+    square.addEventListener("mouseover", () => square.classList.add('black')));
+resetButton.addEventListener("click", resetGrid)
 
-let squareElem;
-for (i=1; i <= numOfSquares; i++) {
+function drawGrid(dimensions) {
+    let numOfSquares = Math.pow(dimensions, 2);
+    let squareElem;
+    for (i=1; i <= numOfSquares; i++) {
     squareElem = document.createElement('div');
     squareElem.classList.add('square');
+    // squareElem.textContent = `${i}`;s //for seeing problems with columns and rows
     container.appendChild(squareElem);
+    }
 }
-const squares = document.querySelectorAll('.square');
-squares.forEach((square) => 
-    square.addEventListener("mouseover", () => square.classList.add('black')))
+function resetGrid() {
+    squares.forEach(square => container.removeChild(square));
+    let userDimensions = prompt("Enter in dimensions of the grid (max 100: ", 16);
+    if (userDimensions > 100) userDimensions = 100;
+    drawGrid(userDimensions)
+    squares = document.querySelectorAll('.square');
+    squares.forEach((square) => 
+        square.addEventListener("mouseover", () => square.classList.add('black')));
+}
 
-// Write an eventListener onto the button for click
-resetButton.addEventListener("click", () => 
-    // On click, foreach square in squares, run square.ClassList.remove('black)
-    squares.forEach(square => square.classList.remove('black')))
 
 
 
