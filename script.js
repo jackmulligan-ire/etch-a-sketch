e675a7c5d4d102c55ff1a73b134993ed3491d2b2
Jack Mulligan <mulligja1@gmail.com>
2021-08-31 08:54:02 +0100

    Reformatted script.js

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 const container = document.querySelector('#container');
 const resetButton = document.getElementById('reset-button');
 const initDimensions = 16;
+let squares; 
 drawGrid(initDimensions)
-let squares = document.querySelectorAll('.square');
-// Adding the event listener for the initial squares
-squares.forEach((square) => 
-    square.addEventListener("mouseover", () => square.classList.add('black')));
 resetButton.addEventListener("click", resetGrid)
 
 function drawGrid(dimensions) {
     let numOfSquares = Math.pow(dimensions, 2);
     let squareElem;
     for (i=1; i <= numOfSquares; i++) {
-    squareElem = document.createElement('div');
-    squareElem.classList.add('square');
-    // squareElem.textContent = `${i}`;s //for seeing problems with columns and rows
-    container.appendChild(squareElem);
+        squareElem = document.createElement('div');
+        squareElem.classList.add('square');
+        // squareElem.textContent = `${i}`;s //for seeing problems with columns and rows
+        container.appendChild(squareElem);
     }
+    squares = document.querySelectorAll('.square');
+    squares.forEach((square) => 
+        square.addEventListener("mouseover", () => square.classList.add('black')));
 }
 function resetGrid() {
     squares.forEach(square => container.removeChild(square));
