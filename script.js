53ead5882191ba99eec5f640002c906cbd17be00
Jack Mulligan <mulligja1@gmail.com>
2021-08-31 11:21:57 +0100

    Even square sizes for all dimensions

--- script.js
+++ script.js
@@ -7,11 +7,18 @@ resetButton.addEventListener("click", resetGrid)
 
 function drawGrid(dimensions) {
     let numOfSquares = Math.pow(dimensions, 2);
-    let squareElem;
+    // Declare flexValue = 100/dimensions.toFixed(3);
+    let flexValue = 100/dimensions.toFixed(3);
+    // Declare flexDeclaration = flexValue + "%"
+    let flexDeclaration = flexValue + "%";
+    // Declare var squareStyle
+    let squareElem
     for (i=1; i <= numOfSquares; i++) {
         squareElem = document.createElement('div');
         squareElem.classList.add('square');
-        // squareElem.textContent = `${i}`;s //for seeing problems with columns and rows
+        // declare flex of squareElem to be flexDeclaration
+        squareElem.style.flex = flexDeclaration;
+        //squareElem.textContent = `${i}`; //for seeing problems with columns and rows
         container.appendChild(squareElem);
     }
     squares = document.querySelectorAll('.square');
@@ -20,12 +27,9 @@ function drawGrid(dimensions) {
 }
 function resetGrid() {
     squares.forEach(square => container.removeChild(square));
-    let userDimensions = prompt("Enter in dimensions of the grid (max 100: ", 16);
+    let userDimensions = +prompt("Enter in dimensions of the grid (max 100: ", 16);
     if (userDimensions > 100) userDimensions = 100;
     drawGrid(userDimensions)
-    squares = document.querySelectorAll('.square');
-    squares.forEach((square) => 
-        square.addEventListener("mouseover", () => square.classList.add('black')));
 }
 
 
