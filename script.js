66541438703b0f746c7f283eb110e7e6b90206ba
Jack Mulligan <mulligja1@gmail.com>
2021-09-01 08:31:46 +0100

    Semicolon refactor

--- script.js
+++ script.js
@@ -11,23 +11,23 @@ function drawGrid(dimensions) {
     let numOfSquares = Math.pow(dimensions, 2);
     let flexValue = 100/dimensions.toFixed(3);
     let flexDeclaration = flexValue + "%";
-    let square
+    let square;
     for (i=0; i < numOfSquares; i++) {
-        square = document.createElement('div');
-        square.classList.add('square');
-        square.style.flex = flexDeclaration;
-        square.addEventListener("mouseover", (e) => e.target.classList.add('black'));
-        container.appendChild(square);
+        square = document.createElement('div')
+        square.classList.add('square')
+        square.style.flex = flexDeclaration
+        square.addEventListener("mouseover", (e) => e.target.classList.add('black'))
+        container.appendChild(square)
     }
-    dimensionDiv.textContent = `Dimensions: ${dimensions} x ${dimensions}`;
-    squareDiv.textContent = `No. of Squares: ${numOfSquares}`;
+    dimensionDiv.textContent = `Dimensions: ${dimensions} x ${dimensions}`
+    squareDiv.textContent = `No. of Squares: ${numOfSquares}`
 }
 
 function resetGrid() {
     let squares = document.querySelectorAll('.square');
-    squares.forEach(square => container.removeChild(square));
+    squares.forEach(square => container.removeChild(square))
     let userDimensions = +prompt("Enter in dimensions of the grid (max 100): ", 16);
-    if (userDimensions > 100) userDimensions = 100;
+    if (userDimensions > 100) userDimensions = 100
     drawGrid(userDimensions)
 }
 
